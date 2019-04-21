c78d15b3066359d174076b82c8cdf6556b59ab05
Aleksey <aleksey.bestuzhev@gmail.com>
2019-04-21 23:41:33 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?require_once 'template/header.php';?>
+
+<div class="wrapper">
+    <div class="app-entrance">
+    <form action="auh.php" class="c-form app-entrance-form">
+        <div class="c-form-item">
+            <label for="app-entrance-form__name" class="c-form-label">Логин</label>
+            <input type="text" class="c-form-input">
+        </div>
+    </form>
+    </div>
+</div>
+
+<?require_once 'template/footer.php'; ?>
